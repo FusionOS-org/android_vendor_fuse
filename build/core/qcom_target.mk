dasshubham762/vendor_derp

vendor: Import build additions from POSP
Change-Id: I83ceb30c3d03212876a328c57bcf038e998fa007
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

build/core/qcom_target.mk
@@ -1,0 +1,30 @@
+# Bring in Qualcomm helper macros
+include vendor/derp/build/core/qcom_utils.mk
+
+# Set device-specific HALs into project pathmap
+define set-device-specific-path
+$(if $(USE_DEVICE_SPECIFIC_$(1)), \
+    $(if $(DEVICE_SPECIFIC_$(1)_PATH), \
+        $(eval path := $(DEVICE_SPECIFIC_$(1)_PATH)), \
+        $(eval path := $(TARGET_DEVICE_DIR)/$(2))), \
+    $(eval path := $(3))) \
+$(call project-set-path,qcom-$(2),$(strip $(path)))
+endef
+
+ifeq ($(PRODUCT_USES_QCOM_HARDWARE),true)
+
+$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
+$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
+$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
+
+$(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
+$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
+$(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
+$(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
+$(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
+$(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
+
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
+
+endif

build/soong/generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func derpExpandVariables(ctx android.ModuleContext, in string) string {
+	derpVars := ctx.Config().VendorConfig("derpVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if derpVars.IsSet(name) {
+			return derpVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}

build/soong/soong_config.mk
@@ -1,0 +1,29 @@
+add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
+
+_json_contents := $(_json_contents)    "Derp":{$(newline)
+
+$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
+$(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_hwfde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
+$(call add_json_bool, Device_support_legacy_hwfde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_wait_for_qsee, $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
+$(call add_json_str, Java_Source_Overlays, $(JAVA_SOURCE_OVERLAYS))
+$(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_str_omitempty, Qcom_um_soong_namespace, $(if $(filter $(UM_PLATFORMS),$(PRODUCT_BOARD_PLATFORM)),$(QCOM_SOONG_NAMESPACE),))
+$(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
+$(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+$(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Target_needs_netd_direct_connect_rule, $(filter true,$(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)))
+$(call add_json_str_omitempty, Target_surfaceflinger_fod_lib, $(TARGET_SURFACEFLINGER_FOD_LIB))
+$(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
+$(call add_json_bool, Target_ignores_ftp_pptp_conntrack_failure, $(filter true,$(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)))
+$(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_json_contents := $(_json_contents)__SV_END
+
+_json_contents := $(_json_contents)    },$(newline)

build/tools/buildinfo.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "# begin derp build properties"
+echo "# autogenerated by buildinfo.sh (derp)"
+
+GET_VERSION="vendor/derp/build/tools/getversion.py"
+echo "ro.build.project=derp"
+echo "ro.derp.branding.version=$($GET_VERSION buildtype)"
+echo "ro.derp.buildtype=$($GET_VERSION buildtype)"
+echo "ro.derp.status=$($GET_VERSION status)"
+echo "ro.derp.version=$($GET_VERSION version)"
+echo "ro.build.datetime=`$DATE +%s`"
+
+echo "# end derp build properties"

build/tools/getversion.py
@@ -1,0 +1,73 @@
+#!/usr/bin/env python2.7
+
+import sys
+
+import json
+import os
+import subprocess
+
+
+def is_subdir(a, b):
+    a = os.path.realpath(a) + '/'
+    b = os.path.realpath(b) + '/'
+    return b == a[:len(b)]
+
+
+def get_build_type(target_product):
+    build_type = os.environ['BUILD_TYPE'].strip() if 'BUILD_TYPE' in os.environ else ''
+    current_device = target_product.split("_")[1]
+    if build_type == 'OFFICIAL' or build_type == 'CUCKED':
+        with open("vendor/derp/derp.devices", "r") as read_file:
+            devices = read_file.read().splitlines()
+            if current_device not in devices:
+                build_type = 'Community'
+            else:
+                build_type = build_type.title()
+    else:
+        build_type = "Community"
+    return build_type
+
+
+def ensure_path():
+    if 'ANDROID_BUILD_TOP' in os.environ:
+        top = os.environ['ANDROID_BUILD_TOP']
+        if not is_subdir(os.getcwd(), top):
+            sys.stderr.write('ERROR: You must run this tool from within $ANDROID_BUILD_TOP!\n')
+            sys.exit(1)
+        os.chdir(os.environ['ANDROID_BUILD_TOP'])
+
+    if not os.path.isdir('.repo'):
+        sys.stderr.write('ERROR: No .repo directory found. Please run this from the top of your tree.\n')
+        sys.exit(1)
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.stderr.write('ERROR: You must provide a query!\n')
+        sys.exit(1)
+
+    ensure_path()
+    query = sys.argv[1]
+    data = {}
+    with open("vendor/derp/version.json", "r") as read_file:
+        data = json.load(read_file)
+
+    target_product = os.environ['TARGET_PRODUCT'] if 'TARGET_PRODUCT' in os.environ else ''
+    date = subprocess.Popen(['date', '-u', '+%Y%m%d'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT).communicate()[0].strip()
+    data['product'] = target_product
+    data['device'] = target_product.split("_")[1]
+    data['date'] = date
+
+    if query == 'buildtype':
+        print(get_build_type(target_product))
+    elif query == 'version':
+        print("{}-{}-{}-{}.{}".format(data['product'], data['platform'], date, data['status'],
+                                          get_build_type(target_product)))
+    else:
+        print(data[query] if query in data.keys() else '')
+
+
+if __name__ == '__main__':
+    main()

FusionOS-org/android_vendor_fuse

Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
Signed-off-by: TextsFromYourEx <skgaming7689@gmail.com>

build/core/qcom_target.mk
@@ -1,0 +1,31 @@
+# Bring in Qualcomm helper macros
+include vendor/fuse/build/core/qcom_utils.mk
+
+# Set device-specific HALs into project pathmap
+define set-device-specific-path
+$(if $(USE_DEVICE_SPECIFIC_$(1)), \
+    $(if $(DEVICE_SPECIFIC_$(1)_PATH), \
+        $(eval path := $(DEVICE_SPECIFIC_$(1)_PATH)), \
+        $(eval path := $(TARGET_DEVICE_DIR)/$(2))), \
+    $(eval path := $(3))) \
+$(call project-set-path,qcom-$(2),$(strip $(path)))
+endef
+
+ifeq ($(PRODUCT_USES_QCOM_HARDWARE),true)
+
+$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
+$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
+$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
+
+$(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
+$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
+$(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
+$(call set-device-specific-path,POWER,power,hardware/qcom-caf/power)
+$(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
+$(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
+$(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
+
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
+
+endif

build/soong/generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func fuseExpandVariables(ctx android.ModuleContext, in string) string {
+	fuseVars := ctx.Config().VendorConfig("fuseVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if fuseVars.IsSet(name) {
+			return fuseVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}

build/soong/soong_config.mk
@@ -1,0 +1,29 @@
+add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
+
+_json_contents := $(_json_contents)    "Fuse":{$(newline)
+
+$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
+$(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_hwfde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
+$(call add_json_bool, Device_support_legacy_hwfde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_wait_for_qsee, $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
+$(call add_json_str, Java_Source_Overlays, $(JAVA_SOURCE_OVERLAYS))
+$(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_str_omitempty, Qcom_um_soong_namespace, $(if $(filter $(UM_PLATFORMS),$(PRODUCT_BOARD_PLATFORM)),$(QCOM_SOONG_NAMESPACE),))
+$(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
+$(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+$(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Target_needs_netd_direct_connect_rule, $(filter true,$(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)))
+$(call add_json_str_omitempty, Target_surfaceflinger_fod_lib, $(TARGET_SURFACEFLINGER_FOD_LIB))
+$(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
+$(call add_json_bool, Target_ignores_ftp_pptp_conntrack_failure, $(filter true,$(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)))
+$(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_json_contents := $(_json_contents)__SV_END
+
+_json_contents := $(_json_contents)    },$(newline)

build/tools/buildinfo.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "# begin fuse build properties"
+echo "# autogenerated by buildinfo.sh (fuse)"
+
+GET_VERSION="vendor/fuse/build/tools/getversion.py"
+echo "ro.build.project=fuse"
+echo "ro.fuse.branding.version=$($GET_VERSION buildtype)"
+echo "ro.fuse.buildtype=$($GET_VERSION buildtype)"
+echo "ro.fuse.status=$($GET_VERSION status)"
+echo "ro.fuse.version=$($GET_VERSION version)"
+echo "ro.build.datetime=`$DATE +%s`"
+
+echo "# end fuse build properties"

build/tools/getversion.py
@@ -1,0 +1,73 @@
+#!/usr/bin/env python2.7
+
+import sys
+
+import json
+import os
+import subprocess
+
+
+def is_subdir(a, b):
+    a = os.path.realpath(a) + '/'
+    b = os.path.realpath(b) + '/'
+    return b == a[:len(b)]
+
+
+def get_build_type(target_product):
+    build_type = os.environ['BUILD_TYPE'].strip() if 'BUILD_TYPE' in os.environ else ''
+    current_device = target_product.split("_")[1]
+    if build_type == 'Official' :
+        with open("vendor/fuse/fuse.devices", "r") as read_file:
+            devices = read_file.read().splitlines()
+            if current_device not in devices:
+                build_type = 'Unofficial'
+            else:
+                build_type = build_type.title()
+    else:
+        build_type = "Unofficial"
+    return build_type
+
+
+def ensure_path():
+    if 'ANDROID_BUILD_TOP' in os.environ:
+        top = os.environ['ANDROID_BUILD_TOP']
+        if not is_subdir(os.getcwd(), top):
+            sys.stderr.write('ERROR: You must run this tool from within $ANDROID_BUILD_TOP!\n')
+            sys.exit(1)
+        os.chdir(os.environ['ANDROID_BUILD_TOP'])
+
+    if not os.path.isdir('.repo'):
+        sys.stderr.write('ERROR: No .repo directory found. Please run this from the top of your tree.\n')
+        sys.exit(1)
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.stderr.write('ERROR: You must provide a query!\n')
+        sys.exit(1)
+
+    ensure_path()
+    query = sys.argv[1]
+    data = {}
+    with open("vendor/fuse/version.json", "r") as read_file:
+        data = json.load(read_file)
+
+    target_product = os.environ['TARGET_PRODUCT'] if 'TARGET_PRODUCT' in os.environ else ''
+    date = subprocess.Popen(['date', '-u', '+%Y%m%d'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT).communicate()[0].strip()
+    data['product'] = target_product
+    data['device'] = target_product.split("_")[1]
+    data['date'] = date
+
+    if query == 'buildtype':
+        print(get_build_type(target_product))
+    elif query == 'version':
+        print("{}-{}-{}-{}.{}".format(data['product'], data['platform'], date, data['status'],
+                                          get_build_type(target_product)))
+    else:
+        print(data[query] if query in data.keys() else '')
+
+
+if __name__ == '__main__':
+    main()